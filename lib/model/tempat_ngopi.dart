826598da35d6ec12283148d42e0a91f7a62b9b35
Rahmad18-10 <rahmad.ramadhan.spk123@gmail.com>
2026-03-16 02:55:17 +0800

    Update tempat_ngopi.dart

--- lib/model/tempat_ngopi.dart
+++ lib/model/tempat_ngopi.dart
@@ -1,11 +1,24 @@
 class TempatNgopi {
-  String nama;
-  String lokasi;
-  String catatan;
+
+  final String? id;
+  final String nama;
+  final String lokasi;
+  final String catatan;
 
   TempatNgopi({
+    this.id,
     required this.nama,
     required this.lokasi,
     required this.catatan,
   });
+
+  factory TempatNgopi.fromMap(Map<String, dynamic> map) {
+    return TempatNgopi(
+      id: map['id']?.toString(),
+      nama: map['nama'] ?? '',
+      lokasi: map['lokasi'] ?? '',
+      catatan: map['catatan'] ?? '',
+    );
+  }
+
 }
